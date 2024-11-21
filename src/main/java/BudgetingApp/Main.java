046ce415c8dd7e7733ffa25a2bd1b610d4ffdcf4
Aldebaraan <aldecanedo97@gmail.com>
2024-11-21 10:25:11 -0500

    User Interface -> WIP

--- src/main/java/BudgetingApp/Main.java
+++ src/main/java/BudgetingApp/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // initializes an account with "default" for name and $0 for balance
         ArrayList<Account> accounts = new ArrayList<>();
         double depositAmount;
+        double netWorth = 0;
 
         // Default accounts named default and with $0 balance
         Account credit = accounts.getFirst();
@@ -18,11 +19,23 @@ public class Main {
         credit.deposit(1000);
         credit.withdraw(500);
 
-        // This method will make a withdrawal from debit and then that same amount
+        // This static method will make a withdrawal from debit and then that same amount
         // will be deposited into the target account.
         // This method should take an account type and a double type as arguments.
         debit.transfer(depositAmount, savings);
 
+        // Print net worth by adding the balance of each account
+        for (Account account : accounts) {
+           netWorth = account.getBalance();
+        }
 
+        System.out.println("Enter 'Q' at any moment to quit.");
+        System.out.println("Net worth is: " + netWorth);
+
+        // geName() should return a String with the name of the account
+        for (Account account : accounts) {
+            System.out.println(account.getName() + " balance is " + account.getBalance());
+        }
+        System.out.println("What operation do you want to to today?: ");
     }
 }
