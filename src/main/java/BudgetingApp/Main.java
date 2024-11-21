c9c0020f42a0a5646c91f1b29b3d57c548770436
Aldebaraan <aldecanedo97@gmail.com>
2024-11-21 10:15:08 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/org/example/budgetingapp/HelloApplication.java

--- src/main/java/BudgetingApp/Main.java
+++ src/main/java/BudgetingApp/Main.java
@@ -1,7 +1,28 @@
 package BudgetingApp;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Account account = new Account();
+        // Account class needs a constructor that takes no arguments and
+        // initializes an account with "default" for name and $0 for balance
+        ArrayList<Account> accounts = new ArrayList<>();
+        double depositAmount;
+
+        // Default accounts named default and with $0 balance
+        Account credit = accounts.getFirst();
+        Account debit = accounts.get(1);
+        Account savings = accounts.get(2);
+
+        // we need a deposit/withdraw methods that take a double as an argument
+        credit.deposit(1000);
+        credit.withdraw(500);
+
+        // This method will make a withdrawal from debit and then that same amount
+        // will be deposited into the target account.
+        // This method should take an account type and a double type as arguments.
+        debit.transfer(depositAmount, savings);
+
+
     }
 }
