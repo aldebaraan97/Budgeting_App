f6261d6c177c89bad58a59ad4ebd7b11fdc4a744
Aldebaraan <aldecanedo97@gmail.com>
2024-11-28 14:36:06 -0500

    User Interface -> WIP

--- src/main/java/BudgetingApp/Main.java
+++ src/main/java/BudgetingApp/Main.java
@@ -4,41 +4,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Account> accounts = new ArrayList<>();
-        double depositAmount = 0;
-        double netWorth = 0;
 
-        // Default accounts named default and with $0 balance
-        Account credit = new Account("Credit", 0);
-        Account debit = new Account("Debit", 0);
-        Account savings = new Account("Savings", 0);
-
-        accounts.add(credit);
-        accounts.add(debit);
-        accounts.add(savings);
-
-        // we need a deposit/withdraw methods that take a double as an argument
-        credit.setAmountBudgeted(1000);
-        credit.addExpense(500);
-
-        // This static method will make a withdrawal from debit and then that same amount
-        // will be deposited into the target account.
-        // This method should take an account type and a double type as arguments.
-//        debit.transfer(savings, depositAmount);
-
-        // Calculates net worth by adding the balance of each account
-        for (Account account : accounts) {
-           netWorth += account.getAmountBudgeted();
-           netWorth -= account.getAmountSpent();
-        }
-
-        System.out.println("Enter 'Q' at any moment to quit.");
-        System.out.println("Net worth is: " + netWorth);
-
-        // geName() should return a String with the name of the account
-        for (Account account : accounts) {
-            System.out.println(account.getCategoryName() + " balance is " + account.getAmountBudgeted());
-        }
-        System.out.println("What operation do you want to today?: ");
     }
 }
