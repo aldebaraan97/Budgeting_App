40c7a7f4d72d1a1218f9b366994d2670df4c4ba2
Aldebaraan <aldecanedo97@gmail.com>
2024-11-28 15:31:43 -0500

    User Interface -> WIP

--- src/main/java/BudgetingApp/Main.java
+++ src/main/java/BudgetingApp/Main.java
@@ -1,9 +1,34 @@
 package BudgetingApp;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Account> budgets = new ArrayList<>();
+        boolean flag = true;
+        Scanner keyboard = new Scanner(System.in);
+        String input;
 
+        while (flag) {
+            System.out.println("Press Q at any point to quit.");
+            if (budgets.isEmpty()) System.out.println("There are no budgets registered.");
+
+            for (Account account : budgets) {
+                System.out.println(account.getCategoryName());
+                System.out.println(account.getRemainingBudget());
+            }
+            System.out.println("What operation would you like to do?");
+            System.out.println("1. Add budget");
+            System.out.println("2. Remove budget");
+            System.out.println("3. Display budgets");
+            System.out.println("4. Add expense to a budget");
+            System.out.println("Q. Exit\n");
+            input = keyboard.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                flag = false;
+            }
+
+        }
     }
 }
